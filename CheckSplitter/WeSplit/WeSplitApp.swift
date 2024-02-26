76c4c20501ca6ed6ca76f897403ca406dec00ce7
TrevorKimble <117454217+TrevorKimble@users.noreply.github.com>
2024-02-26 11:38:16 -0500

    Add files via upload

---
+++ CheckSplitter/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Trevor Kimble on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
