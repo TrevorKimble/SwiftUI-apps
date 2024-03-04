896557b883d2a1b1ccc34db7bf0900e84621aaa7
TrevorKimble <117454217+TrevorKimble@users.noreply.github.com>
2024-03-04 08:55:58 -0500

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Trevor Kimble on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
